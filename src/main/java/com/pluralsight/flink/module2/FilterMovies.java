7831c3262a2d2bed8b0bc696be5c54732e0f66dc
Bhola-B2C <bholajrs@gmail.com>
2020-08-01 19:52:27 +0530

    Initial commit

---
+++ src/main/java/com/pluralsight/flink/module2/FilterMovies.java
@@ -0,0 +1,66 @@
+package com.pluralsight.flink.module2;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple3;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FilterMovies {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<Tuple3<Long, String, String>> lines = env.readCsvFile("ml-latest-small/movies.csv")
+                .ignoreFirstLine()
+                .parseQuotedStrings('"')
+                .ignoreInvalidLines()
+                .types(Long.class, String.class, String.class);
+
+        DataSet<Movie> movies = lines.map(new MapFunction<Tuple3<Long, String, String>, Movie>() {
+            @Override
+            public Movie map(Tuple3<Long, String, String> csvLine) throws Exception {
+                String movieName = csvLine.f1;
+                String[] genres = csvLine.f2.split("\\|");
+                return new Movie(movieName, new HashSet<>(Arrays.asList(genres)));
+            }
+        });
+
+        DataSet<Movie> filteredMovies = movies.filter(new FilterFunction<Movie>() {
+            @Override
+            public boolean filter(Movie movie) throws Exception {
+                return movie.getGenres().contains("Drama");
+            }
+        });
+
+        filteredMovies.writeAsText("filter-output");
+        env.execute();
+    }
+}
+
+class Movie {
+    private String name;
+    private Set<String> genres;
+
+    public Movie(String name, Set<String> genres) {
+        this.name = name;
+        this.genres = genres;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<String> getGenres() {
+        return genres;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "name='" + name + '\'' +
+                ", genres=" + genres +
+                '}';
+    }
+}
